db6a6a6a07176da97b0abe5212f4ccd542912697
liltexbaby <j.pinto@standarduserllc.com>
2021-09-29 16:05:47 -0400

    First Commit

---
+++ pages/products/[slug].js
@@ -0,0 +1,70 @@
+import Head from 'next/head'
+
+import { fromImageToUrl, API_URL } from '../../utils/urls'
+import { twoDecimals } from '../../utils/format'
+
+const Product = ({product}) => {
+
+    return (
+        <div className="contentContainer">
+            <Head>
+                {product.meta_title &&
+                    <title>{product.meta_title}</title>
+                    
+                }
+                {product.meta_description &&
+                    <meta name="description" content={product.meta_description}/>
+                    
+                }
+
+
+            </Head>
+            <h3>{product.name}</h3>
+            <img src={fromImageToUrl(product.image)}/>
+            <h3>{product.name}</h3>
+            <p>${twoDecimals(product.price)}</p>
+            
+            <p>
+                {product.content}
+            </p>
+        </div>
+
+
+    )
+
+
+}
+
+export async function getStaticProps({params:{slug}}){
+  const product_res = await fetch(`${API_URL}/products/?slug=${slug}`)
+  const found = await product_res.json()
+  
+  return {
+      props:{
+          product: found[0]
+      }
+  }
+}
+
+
+export async function getStaticPaths(){
+    //Retrieve all possible paths
+
+    const products_res = await fetch(`${API_URL}/products/`)
+    const products = await products_res.json()
+
+    //Return them to NextJS context
+
+    return {
+        paths: products.map(product=>({
+            params: { slug: String(product.slug)}
+
+        })),
+        fallback: false
+    }
+
+
+}
+
+
+export default Product
